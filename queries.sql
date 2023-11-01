684f34fabece8282d7fdba58d9e181fc928e7aa0
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-01 15:52:48 +0630

    Write select queries

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
-/*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name = 'Luna';
+Select * from animals where name like '%mon';
+Select name from animals where date_of_birth BETWEEN '2016-1-1' and '2019-1-1';
+Select name from animals where neutered = true And escape_attempts < 3;
+Select date_of_birth from animals where name in ('Agumon', 'Pikachu');
+Select name, escape_attempts from animals where weight_kg > 10.5;
+Select * from animals where neutered = true;
+Select * from animals where name != 'Gabumon';
+select * from animals where weight_kg <= 17.3 and weight_kg >=10.4;
