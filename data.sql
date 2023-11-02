89d8fdac19623e01334a5d17d359a7cb16ab15dc
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-11-02 21:35:47 +0630

    Add data, queries and schema related with transactions, Aggregate functions and Group by

--- data.sql
+++ data.sql
@@ -6,3 +6,18 @@ Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 values (3, 'Pikachu', '2021-1-7', 1, false, 15.04);
 Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 values (4, 'Devimon', '2017-5-12', 5, true, 11);
+
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (5, 'Charmander', '2020-2-8', 0, false, 11, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (6, 'Plantmon', '2021-11-15', 2, true, 5.7, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (7, 'Squirtle', '1993-4-2', 3, false, 12.13, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (8, 'Angemon', '2005-1-12', 1, true, 45, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (9, 'Boarmon', '2005-1-7', 7, true, 20.4, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (10, 'Blossom', '1998-10-13', 3, true, 17, null);
+Insert into animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg, species)
+values (11, 'Ditto', '2022-10-13', 4, true, 22, null);
--- queries.sql
+++ queries.sql
@@ -6,3 +6,85 @@ Select name, escape_attempts from animals where weight_kg > 10.5;
 Select * from animals where neutered = true;
 Select * from animals where name != 'Gabumon';
 select * from animals where weight_kg <= 17.3 and weight_kg >=10.4;
+
+-- Unspecified species
+Begin
+	Update animals 
+	Set species = 'unspecified';
+	Select * from animals;
+	Rollback;
+	
+Select * from animals;
+
+-- Species data
+Begin;
+	Update animals
+	Set species = 'digimon'
+	Where name like '%mon';
+
+	Update animals
+	Set species = 'pokemon'
+	Where name not like '%mon';
+
+	Select * from animals;
+	Commit;
+	
+Select * from animals;
+
+-- Rollback
+Begin;
+	Delete from animals;
+	Select * from animals;
+	Rollback;
+	
+Select * from animals;
+
+-- Savepoint
+Begin;
+	Delete from animals
+	Where date_of_birth > '2022-1-1';
+	Select * from animals;
+	Savepoint sp1;
+	
+	Update animals
+	Set weight_kg = weight_kg * -1;
+	Select * from animals;
+	Rollback to sp1;
+	
+	Select * from animals;
+	
+	Update animals
+	Set weight_kg = weight_kg * -1
+	Where weight_kg < 0;
+	
+	Select * from animals;
+Commit;
+
+Select * from animals;
+
+-- Aggregate functions and Group by
+Select count(id) from animals;
+Select count(*) as Home_owner from animals
+Where escape_attempts = 0;
+
+Select Avg(weight_kg) from animals;
+
+Select neutered, Count(*) as escape_count
+From animals
+Where escape_attempts > 0
+Group by neutered
+Order by escape_count DESC;
+
+Select species,
+	Min(weight_kg) as minimal_weight, 
+	Max(weight_kg) as maximal_weight
+From animals
+Group by species;
+
+Select species, 
+	Avg(escape_attempts) as avg_escape_attempts
+From animals
+Where date_of_birth Between '1990-1-1' and '2000-1-1'
+Group by species;
+
+Select * from animals;
